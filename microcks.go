b420401767c9596147a276620606f0ee123f445f
Laurent Broudoux <laurent.broudoux@gmail.com>
2023-11-06 10:28:27 -0600

    feat: #2 Adding contract-testing :tube: Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- microcks.go
+++ microcks.go
@@ -18,6 +18,7 @@ package microcks
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -25,6 +26,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	client "github.com/microcks/microcks-go-client"
 	"github.com/testcontainers/testcontainers-go"
@@ -40,26 +42,110 @@ type MicrocksContainer struct {
 	testcontainers.Container
 }
 
+// RunContainer creates an instance of the MicrocksContainer type.
+func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*MicrocksContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        defaultImage,
+		ExposedPorts: []string{defaultHttpPort, defaultGrpcPort},
+		WaitingFor:   wait.ForLog("Started MicrocksApplication"),
+	}
+	genericContainerReq := testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	}
+
+	for _, opt := range opts {
+		opt.Customize(&genericContainerReq)
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MicrocksContainer{Container: container}, nil
+}
+
+// HttpEndpoint allows retrieving the Http endpoint where Microcks can be accessed
+// (you'd have to append '/api' to access APIs)
 func (container *MicrocksContainer) HttpEndpoint(ctx context.Context) string {
 	ip, _ := container.Host(ctx)
 	port, _ := container.MappedPort(ctx, defaultHttpPort)
 	return fmt.Sprintf("http://%s:%s", ip, port.Port())
 }
 
+// SoapMockEndpoint get the exposed mock endpoint for a SOAP Service.
+func (container *MicrocksContainer) SoapMockEndpoint(ctx context.Context, service string, version string) string {
+	return fmt.Sprintf("%s/soap/%s/%s", container.HttpEndpoint(ctx), service, version)
+}
+
+// RestMockEndpoints get the exposed mock endpoint for a REST Service.
 func (container *MicrocksContainer) RestMockEndpoint(ctx context.Context, service string, version string) string {
 	return fmt.Sprintf("%s/rest/%s/%s", container.HttpEndpoint(ctx), service, version)
 }
 
+// GraphQLMockEndpoint get the exposed mock endpoints for a GraphQL Service.
+func (container *MicrocksContainer) GrapQLMockEndpoint(ctx context.Context, service string, version string) string {
+	return fmt.Sprintf("%s/graphql/%s/%s", container.HttpEndpoint(ctx), service, version)
+}
+
+// GrpcMockEndpoint get the exposed mock endpoint for a GRPC Service.
+func (container *MicrocksContainer) GrpcMockEndpoint(ctx context.Context) string {
+	ip, _ := container.Host(ctx)
+	port, _ := container.MappedPort(ctx, defaultGrpcPort)
+	return fmt.Sprintf("grpc://%s:%s", ip, port.Port())
+}
+
+// ImportAsMainArtifact imports an artifact as a primary or main one within the Microcks container.
 func (container *MicrocksContainer) ImportAsMainArtifact(artifactFilePath string) (int, error) {
 	return container.importArtifact(artifactFilePath, true)
 }
 
+// ImportAsSecondaryArtifact imports an artifact as a secondary one within the Microcks container.
 func (container *MicrocksContainer) ImportAsSecondaryArtifact(artifactFilePath string) (int, error) {
 	return container.importArtifact(artifactFilePath, false)
 }
 
-func (container *MicrocksContainer) TestEndpoint() {
+// TestEndpoint launches a conformance test on an endpoint.
+func (container *MicrocksContainer) TestEndpoint(testRequest *client.TestRequest) (*client.TestResult, error) {
+	// Get context and retrieve API endpoint.
+	ctx := context.Background()
+	httpEndpoint := container.HttpEndpoint(ctx)
 
+	// Create Microcks client.
+	c, err := client.NewClientWithResponses(httpEndpoint + "/api")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testResult, err := c.CreateTestWithResponse(ctx, *testRequest)
+	if testResult.HTTPResponse.StatusCode == 201 {
+		// Retrieve Id and start polling for final result.
+		var testResultId string = testResult.JSON201.Id
+
+		// Wait an initial delay to avoid inefficient poll.
+		time.Sleep(100 * time.Millisecond)
+
+		// Compute future time that is the end of waiting timeframe.
+		future := nowInMilliseconds() + int64(testRequest.Timeout)
+		for nowInMilliseconds() < future {
+			testResultResponse, err := c.GetTestResultWithResponse(ctx, testResultId)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// If still in progress, then wait again.
+			if testResultResponse.JSON200.InProgress {
+				time.Sleep(200 * time.Millisecond)
+			} else {
+				break
+			}
+		}
+
+		// Return the final result.
+		response, err := c.GetTestResultWithResponse(ctx, testResultId)
+		return response.JSON200, err
+	}
+	return nil, errors.New("Couldn't launch on new test on Microcks. Please check Microcks container logs")
 }
 
 func (container *MicrocksContainer) importArtifact(artifactFilePath string, mainArtifact bool) (int, error) {
@@ -103,26 +189,6 @@ func (container *MicrocksContainer) importArtifact(artifactFilePath string, main
 	return response.StatusCode, err
 }
 
-// RunContainer creates an instance of the MicrocksContainer type.
-func RunContainer(ctx context.Context, opts ...testcontainers.ContainerCustomizer) (*MicrocksContainer, error) {
-	req := testcontainers.ContainerRequest{
-		Image:        defaultImage,
-		ExposedPorts: []string{defaultHttpPort, defaultGrpcPort},
-		WaitingFor:   wait.ForLog("Started MicrocksApplication"),
-	}
-	genericContainerReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	}
-
-	for _, opt := range opts {
-		opt.Customize(&genericContainerReq)
-	}
-
-	container, err := testcontainers.GenericContainer(ctx, genericContainerReq)
-	if err != nil {
-		return nil, err
-	}
-
-	return &MicrocksContainer{Container: container}, nil
+func nowInMilliseconds() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
 }
--- microcks_test.go
+++ microcks_test.go
@@ -28,7 +28,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	client "github.com/microcks/microcks-go-client"
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 func TestMockingFunctionality(t *testing.T) {
@@ -60,22 +62,85 @@ func TestMockingFunctionality(t *testing.T) {
 	testConfigRetrieval(t, ctx, microcksContainer)
 	testMockEndpoints(t, ctx, microcksContainer)
 
-	readCloser, err := microcksContainer.Logs(ctx)
-	// example to read data
-	buf := new(bytes.Buffer)
-
-	numOfByte, err := buf.ReadFrom(readCloser)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	readCloser.Close()
-
-	content := buf.String()
-	fmt.Printf("Read: %d bytes, content is: %q\r\n", numOfByte, content)
-
 	testMicrocksMockingFunctionality(t, ctx, microcksContainer)
+
+	//printMicrocksContainerLogs(ctx, microcksContainer);
+}
+
+func TestContractTestingFunctionnality(t *testing.T) {
+	ctx := context.Background()
+
+	var networkName = "microcks-network"
+	network, err := testcontainers.GenericNetwork(ctx, testcontainers.GenericNetworkRequest{
+		NetworkRequest: testcontainers.NetworkRequest{
+			Name: networkName,
+		},
+	})
+	if err != nil {
+		t.Fatal("Cannot create network")
+	}
+	defer func() {
+		_ = network.Remove(ctx)
+	}()
+
+	// createMicrocksContainer {
+	microcksContainer, err := RunContainer(ctx, customizeMicrocksContainer("quay.io/microcks/microcks-uber:nightly", networkName))
+	require.NoError(t, err)
+
+	badImpl, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:    "quay.io/microcks/contract-testing-demo:01",
+			Networks: []string{networkName},
+			NetworkAliases: map[string][]string{
+				networkName: {"bad-impl"},
+			},
+			WaitingFor: wait.ForLog("Example app listening on port 3001"),
+		},
+		Started: true,
+	})
+	goodImpl, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:    "quay.io/microcks/contract-testing-demo:02",
+			Networks: []string{networkName},
+			NetworkAliases: map[string][]string{
+				networkName: {"good-impl"},
+			},
+			WaitingFor: wait.ForLog("Example app listening on port 3002"),
+		},
+		Started: true,
+	})
+
+	t.Cleanup(func() {
+		if err := microcksContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+		if err := badImpl.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+		if err := goodImpl.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err)
+		}
+	})
+	// }
+
+	// Loading artifacts
+	status, err := microcksContainer.ImportAsMainArtifact("test-resources/apipastries-openapi.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	require.Equal(t, http.StatusCreated, status)
+
+	status, err = microcksContainer.ImportAsSecondaryArtifact("test-resources/apipastries-postman-collection.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	require.Equal(t, http.StatusCreated, status)
+
+	testConfigRetrieval(t, ctx, microcksContainer)
+
+	testMicrocksContractTestingFunctionality(t, ctx, microcksContainer)
+
+	printMicrocksContainerLogs(ctx, microcksContainer)
 }
 
 func testConfigRetrieval(t *testing.T, ctx context.Context, microcksContainer *MicrocksContainer) {
@@ -88,9 +153,20 @@ func testConfigRetrieval(t *testing.T, ctx context.Context, microcksContainer *M
 }
 
 func testMockEndpoints(t *testing.T, ctx context.Context, microcksContainer *MicrocksContainer) {
-	// RestMockEndpoint {
-	baseApiUrl := microcksContainer.RestMockEndpoint(ctx, "API Pastries", "0.0.1")
+	// MockEndpoints {
+	baseApiUrl := microcksContainer.SoapMockEndpoint(ctx, "Pastries Service", "1.0")
+	require.Equal(t, microcksContainer.HttpEndpoint(ctx)+"/soap/Pastries Service/1.0", baseApiUrl)
+
+	baseApiUrl = microcksContainer.RestMockEndpoint(ctx, "API Pastries", "0.0.1")
 	require.Equal(t, microcksContainer.HttpEndpoint(ctx)+"/rest/API Pastries/0.0.1", baseApiUrl)
+
+	baseApiUrl = microcksContainer.GrapQLMockEndpoint(ctx, "Pastries Graph", "1")
+	require.Equal(t, microcksContainer.HttpEndpoint(ctx)+"/graphql/Pastries Graph/1", baseApiUrl)
+
+	baseGrpcUrl := microcksContainer.GrpcMockEndpoint(ctx)
+	ip, _ := microcksContainer.Host(ctx)
+	port, _ := microcksContainer.MappedPort(ctx, defaultGrpcPort)
+	require.Equal(t, "grpc://"+ip+":"+port.Port(), baseGrpcUrl)
 	// }
 }
 
@@ -127,4 +203,60 @@ func testMicrocksMockingFunctionality(t *testing.T, ctx context.Context, microck
 	json.Unmarshal([]byte(body), &pastry)
 
 	require.Equal(t, "Eclair Chocolat", pastry["name"])
+}
+
+func testMicrocksContractTestingFunctionality(t *testing.T, ctx context.Context, microcksContainer *MicrocksContainer) {
+	// Build a new TestRequest.
+	testRequest := client.TestRequest{
+		ServiceId:    "API Pastries:0.0.1",
+		RunnerType:   client.TestRunnerTypeOPENAPISCHEMA,
+		TestEndpoint: "http://bad-impl:3001",
+		Timeout:      2000,
+	}
+
+	testResult, err := microcksContainer.TestEndpoint(&testRequest)
+	require.NoError(t, err)
+
+	println(testResult.Success)
+
+	require.False(t, testResult.Success)
+	require.Equal(t, "http://bad-impl:3001", testResult.TestedEndpoint)
+	//require.Equal(t, 3, testResult.TestCaseResults.size)
+	//require.True(t, testResult.TestCaseResults[0].T)
+
+	// Switch endpoint to the correct implementation.
+	testRequest = client.TestRequest{
+		ServiceId:    "API Pastries:0.0.1",
+		RunnerType:   client.TestRunnerTypeOPENAPISCHEMA,
+		TestEndpoint: "http://good-impl:3002",
+		Timeout:      2000,
+	}
+
+	testResult, err = microcksContainer.TestEndpoint(&testRequest)
+	require.NoError(t, err)
+
+	println(testResult.Success)
+
+	require.True(t, testResult.Success)
+	require.Equal(t, "http://good-impl:3002", testResult.TestedEndpoint)
+}
+
+func customizeMicrocksContainer(image string, network string) testcontainers.CustomizeRequestOption {
+	return func(req *testcontainers.GenericContainerRequest) {
+		req.Image = image
+		req.Networks = []string{network}
+	}
+}
+
+func printMicrocksContainerLogs(ctx context.Context, microcksContainer *MicrocksContainer) {
+	readCloser, err := microcksContainer.Logs(ctx)
+	// example to read data
+	buf := new(bytes.Buffer)
+	numOfByte, err := buf.ReadFrom(readCloser)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	readCloser.Close()
+	fmt.Printf("Read: %d bytes, content is: %q", numOfByte, buf.String())
 }
